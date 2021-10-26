890e98d49fdb74c9ff522e27e1c68be0159a6060
Breno Arosa <breno.arosa@quintoandar.com.br>
2021-10-26 13:24:28 -0300

    Removes unused completion functions

--- init.lua
+++ init.lua
@@ -20,18 +20,6 @@ local nnoremap = utils.nnoremap
 local inoremap = utils.inoremap
 local vnoremap = utils.vnoremap
 
--- create a completion_nvim table on _G which is visible via
--- v:lua from vimscript
-_G.completion_nvim = {}
-
-function _G.completion_nvim.smart_pumvisible(vis_seq, not_vis_seq)
-  if (fn.pumvisible() == 1) then
-    return termcodes(vis_seq)
-  else
-    return termcodes(not_vis_seq)
-  end
-end
-
 -- General
 ---------------------------------------------------------------------------------------------------
 cmd [[syntax on]]
