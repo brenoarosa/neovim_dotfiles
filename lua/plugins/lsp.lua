ff7b48dafffc76f2566c0e3238cab7c9f284b6a7
Breno Arosa <brenoarosa@hotmail.com>
2023-03-27 19:35:02 -0300

    Adds key bindings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,65 @@
 local nvim_lsp = require("lspconfig")
 local null_ls = require("null-ls")
 
+-- Toggle diagnostics helper
+vim.diagnostic.disable()
+vim.g.diagnostics_active = false
+function _G.toggle_diagnostics()
+  if vim.g.diagnostics_active then
+    vim.g.diagnostics_active = false
+    vim.diagnostic.disable()
+  else
+    vim.g.diagnostics_active = true
+    vim.diagnostic.enable()
+  end
+end
+
+-- copied from: https://github.com/neovim/nvim-lspconfig/tree/c5505c70571b094663745167d40388edd40f6450#suggested-configuration
+-- changed <space> for <leader>
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<learder>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    -- remove since don't know how works
+    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    -- vim.keymap.set('n', '<space>wl', function()
+    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    -- end, opts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+
+    -- my custom maps
+    vim.keymap.set("n", "<leader>c", toggle_diagnostics)
+  end,
+})
+
+
 -- Jedi
 nvim_lsp.jedi_language_server.setup {
   root_dir = function(fname)
@@ -60,18 +119,3 @@ null_ls.setup({
   },
 })
 
--- Toggle diagnostics helper
-vim.diagnostic.disable()
-vim.g.diagnostics_active = false
-function _G.toggle_diagnostics()
-  if vim.g.diagnostics_active then
-    vim.g.diagnostics_active = false
-    vim.diagnostic.disable()
-  else
-    vim.g.diagnostics_active = true
-    vim.diagnostic.enable()
-  end
-end
-
-vim.keymap.set("n", "<leader>c", toggle_diagnostics)
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
